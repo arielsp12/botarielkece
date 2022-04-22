cecf133445012ee1799b9c4a576d5fa49378abc8
arielsp12 <arielsandypermana1@gmail.com>
2022-04-22 09:43:53 +0700

    arielganz

---
+++ install.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/bash
+
+apt-get update
+apt-get upgrade
+apt-get install nodejs
+apt-get install libwebp
+apt-get install mc
+apt-get install ffmpeg
+apt-get install wget
+apt-get install tesseract
+apt-get install nmap
+wget -O ~/../usr/share/tessdata/ind.traineddata "https://github.com/tesseract-ocr/tessdata/blob/master/ind.traineddata?raw=true"
+npm install -g pm2
+pm2 start main.js
+pm2 save
+
+echo "DONE INSTALLING ALL PACKAGE REQUIRE, TINGGAL RUN pm2 monit"
